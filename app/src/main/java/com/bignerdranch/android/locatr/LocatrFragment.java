a8390e92d57d28be2b739dc9fcad0cac2059b5fe
IFang Lee <ilee@ifwe.co>
2017-08-31 16:08:57 -0700

    add runtime permission check

--- app/src/main/java/com/bignerdranch/android/locatr/LocatrFragment.java
+++ app/src/main/java/com/bignerdranch/android/locatr/LocatrFragment.java
@@ -2,11 +2,16 @@ package com.bignerdranch.android.locatr;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -21,9 +26,18 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+import java.io.IOException;
+import java.util.List;
+
 public class LocatrFragment extends Fragment {
     private static final String TAG = "LocatrFragment";
 
+    private static final String[] LOCATION_PERMISSIONS = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+    };
+
+    private static final int REQUEST_LOCATION_PERMISSIONS = 0;
     private ImageView mImageView;
     private GoogleApiClient mClient;
 
@@ -88,10 +102,25 @@ public class LocatrFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_locate:
-                findImage();
+                if (hasLocationPermission()) {
+                    findImage();
+                } else {
+                    requestPermissions(LOCATION_PERMISSIONS,
+                            REQUEST_LOCATION_PERMISSIONS);
+                }
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSIONS:
+                if (hasLocationPermission()) findImage();
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
@@ -108,7 +137,6 @@ public class LocatrFragment extends Fragment {
         request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         request.setNumUpdates(1);
         request.setInterval(0);
-
 
         if (ActivityCompat.checkSelfPermission(this.getContext()
                 , Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
@@ -128,7 +156,42 @@ public class LocatrFragment extends Fragment {
                     @Override
                     public void onLocationChanged(Location location) {
                         Log.i(TAG, "Got a fix: " + location);
+                        new SearchTask().execute(location);
                     }
                 });
+    }
+
+    private boolean hasLocationPermission() {
+        int result = ContextCompat
+                .checkSelfPermission(getActivity(), LOCATION_PERMISSIONS[0]);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private class SearchTask extends AsyncTask<Location,Void,Void> {
+        private GalleryItem mGalleryItem;
+        private Bitmap mBitmap;
+
+        @Override
+        protected Void doInBackground(Location... locations) {
+            FlickrFetcher fetchr = new FlickrFetcher();
+            List<GalleryItem> items = fetchr.searchPhotos(locations[0]);
+            if (items.size() == 0) {
+                return null;
+            }
+            mGalleryItem = items.get(0);
+
+            try {
+                byte[] bytes = fetchr.getUrlBytes(mGalleryItem.getUrl());
+                mBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            } catch (IOException ioe) {
+                Log.i(TAG, "Unable to download bitmap", ioe);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            mImageView.setImageBitmap(mBitmap);
+        }
     }
 }
